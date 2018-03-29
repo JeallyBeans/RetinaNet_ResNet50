c976d9c3d54efcf829fa6b6d0a04ba5af3bfb041
JeallyBeans <37878435+JeallyBeans@users.noreply.github.com>
2018-03-29 21:12:04 +0200

    path nad run fix

--- test_folder/retina_setup.py
+++ test_folder/retina_setup.py
@@ -4,10 +4,10 @@ Debug = False
 
 # Directory where the desired snapshot is stored.
 # os.path.abspath... Creates the path from the parent folder to where this file is stored.
-snapshot_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "/keras-retinanet-master/snapshots/resnet50_pascal_07.h5"
+snapshot_path = (os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "/keras-retinanet-master/snapshots/resnet50_pascal_07.h5")
 
 # Directory to where the data set is stored. Functions the same as snapshot_path over---^
-data_path     =  os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +  "/VOC_2007_2012/VOCtrainval_06-Nov-2007/VOCdevkit/VOC2007")
+data_path     =  (os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +  "/VOC_2007_2012/VOCtrainval_06-Nov-2007/VOCdevkit/VOC2007")
 
 # Arguments to pass along when running the train.py file from keras-retinanet-master repository.
 # More arguments can be found in the train.py file.
@@ -16,16 +16,21 @@ config_train = "--snapshot " + snapshot_path + "  --freeze-backbone --epochs 13 
 # Arguments to pass along the debug.py file. Has the same parameter as the above Str.
 config_debug = "--anchors --annotations "
 
-if Debug = True:
+
+print("------------\n " + data_path)
+
+if Debug == True :
     os.system("cd keras-retinanet-master & " +
         "cd &"                               +
        "keras_retinanet\\bin\\debug.py "     +
         config_debug                         +
        "pascal " + data_path)
 
+
 else:
-    os.system("cd keras-retinanet-master & " +
-         "cd &"                              +
-        "keras_retinanet\\bin\\train.py "    +
+    os.system("cd .."                        +
+        "cd keras-retinanet-master & "       +
+        "cd &"                              +
+        "python keras_retinanet\\bin\\train.py "    +
         config_train                         +
         "pascal " + data_path)
