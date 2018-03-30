4c12e4358f28e4d2e3515ca0106e18413261b93e
JeallyBeans <37878435+JeallyBeans@users.noreply.github.com>
2018-03-30 04:20:44 +0200

    fixed classes error

--- test_folder/retina_setup.py
+++ test_folder/retina_setup.py
@@ -4,14 +4,14 @@ Debug = False
 
 # Directory where the desired snapshot is stored.
 # os.path.abspath... Creates the path from the parent folder to where this file is stored.
-snapshot_path = (os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "/keras-retinanet-master/snapshots/resnet50_pascal_07.h5")
+snapshot_path = (os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "/keras-retinanet-master/snapshots/resnet50_pascal_13.h5")
 
 # Directory to where the data set is stored. Functions the same as snapshot_path over---^
 data_path     =  (os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +  "/VOC_2007_2012/VOCtrainval_06-Nov-2007/VOCdevkit/VOC2007")
 
 # Arguments to pass along when running the train.py file from keras-retinanet-master repository.
 # More arguments can be found in the train.py file.
-config_train = "--snapshot " + snapshot_path + "  --freeze-backbone --epochs 13 --steps 1000 --no-evaluation "
+config_train = " --backbone mobilenet128  --freeze-backbone --epochs 10 --steps 10000 --no-evaluation  " # "--snapshot " + snapshot_path +
 
 # Arguments to pass along the debug.py file. Has the same parameter as the above Str.
 config_debug = "--anchors --annotations "
